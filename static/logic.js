94f5b36996fabec336d74f85671e0baef262c9a9
Andy O'Bryan <andrew.obryan@gmail.com>
2021-10-29 15:52:56 -0400

    updated with bubble heat map and some clarifying information

--- static/logic.js
+++ static/logic.js
@@ -250,110 +250,72 @@ function confirm_premium(){
   }
 }
 
-// Create chart instance
-var chart = am4core.create("chartdiv-XY", am4charts.XYChart);
+// creating the bullet chart heat map for Customer Journey Analytics
+var chart = am4core.create("chartdiv-bubble", am4charts.XYChart);
+chart.maskBullets = false;
 
-// Title
-var title = chart.titles.push(new am4core.Label());
-title.text = "Join/Leave by Audience";
-title.fontSize = 25;
-title.marginBottom = 15;
+// Setting the axes
+var xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+var yAxis = chart.yAxes.push(new am4charts.CategoryAxis());
 
-// Add data
-chart.data = [{
-  "category": "App Fanatics",
-  "negative1": -0.1,
-  "negative2": -0.9,
-  "positive1": 5,
-  "positive2": 94
-}, {
-  "category": "MVPs",
-  "negative1": -2,
-  "negative2": -4,
-  "positive1": 19,
-  "positive2": 75
-}, {
-  "category": "Croc Lovers",
-  "negative1": -2,
-  "negative2": -10,
-  "positive1": 46,
-  "positive2": 42
-}, {
-  "category": "Croc Haters",
-  "negative1": -2,
-  "negative2": -13,
-  "positive1": 33,
-  "positive2": 52
-}, {
-  "category": "Recent Purchasers",
-  "negative1": -6,
-  "negative2": -19,
-  "positive1": 34,
-  "positive2": 41
-}];
+// Binding the category of data to teh axes
+yAxis.dataFields.category = "Cohort";
+xAxis.renderer.minGridDistance = 40;
+xAxis.dataFields.category = "Funnel Stage";
+// This puts tuts the Funnel Stage on top of the graph
+xAxis.renderer.opposite = true;
 
+// Setting the series data
+var series = chart.series.push(new am4charts.ColumnSeries());
+series.dataFields.categoryY = "Cohort";
+series.dataFields.categoryX = "Funnel Stage";
+series.dataFields.value = "Funnel Size";
+// Setting up some placeholders to use in my tooltip later
+series.dataFields.touchpointvalue = "Touchpoints";
+series.dataFields.funnelretained = "Funnel Stage Retained"
+series.dataFields.totalretained = "Total Audience Retained"
+series.columns.template.disabled = true;
+series.sequencedInterpolation = true;
+// Breaks inheritance of Tooltip and sets customer color
+series.tooltip.getFillFromObject = false;
+series.tooltip.background.fill = am4core.color("white");
+series.tooltip.label.fill = am4core.color("black");
 
-// Create axes
-var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "category";
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.renderer.inversed = true;
-categoryAxis.renderer.minGridDistance = 20;
-categoryAxis.renderer.axisFills.template.disabled = false;
-categoryAxis.renderer.axisFills.template.fillOpacity = 0.05;
+var bullet = series.bullets.push(new am4core.Circle());
+bullet.tooltipText = "[bold]Audience Size[/]: {Funnel Size}\nAverage Customer Touchpoints: {touchpointvalue}\nRetained from Previous Stage:{funnelretained}\nRetained Since Initial Interaction:{totalretained}";
+bullet.strokeWidth = 3;
+bullet.stroke = am4core.color("#ffffff");
+bullet.strokeOpacity = 0;
 
-
-var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-valueAxis.min = -100;
-valueAxis.max = 100;
-valueAxis.renderer.minGridDistance = 50;
-valueAxis.renderer.ticks.template.length = 5;
-valueAxis.renderer.ticks.template.disabled = false;
-valueAxis.renderer.ticks.template.strokeOpacity = 0.4;
-valueAxis.renderer.labels.template.adapter.add("text", function(text) {
-  return text + "%";
-})
-
-// Legend
-chart.legend = new am4charts.Legend();
-chart.legend.position = "right";
-
-// Use only absolute numbers
-chart.numberFormatter.numberFormat = "#.#s";
-
-// Create series
-function createSeries(field, name, color) {
-  var series = chart.series.push(new am4charts.ColumnSeries());
-  series.dataFields.valueX = field;
-  series.dataFields.categoryY = "category";
-  series.stacked = true;
-  series.name = name;
-  series.stroke = color;
-  series.fill = color;
-  
-  var label = series.bullets.push(new am4charts.LabelBullet);
-  label.label.text = "{valueX}%";
-  label.label.fill = am4core.color("#fff");
-  label.label.strokeWidth = 0;
-  label.label.truncate = false;
-  label.label.hideOversized = true;
-  label.locationX = 0.5;
-  return series;
-}
-
-var interfaceColors = new am4core.InterfaceColorSet();
-var positiveColor = interfaceColors.getFor("positive");
-var negativeColor = interfaceColors.getFor("negative");
-
-createSeries("negative2", "Unlikely", negativeColor.lighten(0.5));
-createSeries("negative1", "Never", negativeColor);
-createSeries("positive1", "Sometimes", positiveColor.lighten(0.5));
-createSeries("positive2", "Very often", positiveColor);
-
-chart.legend.events.on("layoutvalidated", function(event){
-  chart.legend.itemContainers.each((container)=>{
-    if(container.dataItem.dataContext.name == "Never"){
-      container.toBack();
+bullet.adapter.add("fill", function(fill,target){
+  if(target.dataItem){
+    if(target.dataItem.value >=622)
+      {
+        return am4core.color("green")
+      }
+    else if (target.dataItem.value >=393)
+    {
+      return am4core.color("yellow")
     }
-  })
-})
+    else {
+      return am4core.color("red")
+    }
+  }
+});
+
+series.heatRules.push({
+  property: "radius",
+  target: bullet,
+  min: 2,
+  max: 25
+});
+
+bullet.hiddenState.properties.scale = 0.01;
+bullet.hiddenState.properties.opacity = 1;
+
+chart.scrollbarX = new am4core.Scrollbar();
+chart.scrollbarY = new am4core.Scrollbar();
+
+chart.dataSource.url = "Resources/bubbledata.csv";
+chart.dataSource.parser = new am4core.CSVParser();
+chart.dataSource.parser.options.useColumnNames = true;
